d9dc8e86109f27e9924d0fcceaee42026ad62c08
exokortex <johannes.haring@gmx.net>
2018-01-18 02:25:36 +0100

    do not load key from file

--- CTFd/config.py
+++ CTFd/config.py
@@ -2,13 +2,13 @@ import os
 
 ''' GENERATE SECRET KEY '''
 
-with open('.ctfd_secret_key', 'a+b') as secret:
-    secret.seek(0)  # Seek to beginning of file since a+ mode leaves you at the end and w+ deletes the file
-    key = secret.read()
-    if not key:
-        key = os.urandom(64)
-        secret.write(key)
-        secret.flush()
+#with open('.ctfd_secret_key', 'a+b') as secret:
+#    secret.seek(0)  # Seek to beginning of file since a+ mode leaves you at the end and w+ deletes the file
+#    key = secret.read()
+#    if not key:
+#        key = os.urandom(64)
+#        secret.write(key)
+#        secret.flush()
 
 ''' SERVER SETTINGS '''
 
