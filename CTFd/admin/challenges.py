aa4c3ff731333d075d053b1bd5d00dc2c1b8cda9
exokortex <johannes.haring@gmx.net>
2019-02-06 14:39:10 +0100

    readd json endpoint for all challenges

--- CTFd/admin/challenges.py
+++ CTFd/admin/challenges.py
@@ -1,8 +1,10 @@
-from flask import current_app as app, render_template, render_template_string, url_for
+from flask import current_app as app, render_template, render_template_string, url_for, jsonify
 from CTFd.utils.decorators import admins_only
 from CTFd.utils import binary_type
 from CTFd.models import Solves, Challenges, Flags
+from CTFd.models import db, Teams, Tags, Files, Tracking, Pages, Hints, Unlocks
 from CTFd.plugins.challenges import get_chal_class
+from CTFd.plugins.challenges import CHALLENGE_CLASSES
 from CTFd.admin import admin
 import os
 import six
@@ -13,6 +15,41 @@ import six
 def challenges_listing():
     challenges = Challenges.query.all()
     return render_template('admin/challenges/challenges.html', challenges=challenges)
+
+
+@admin.route('/admin/chals', methods=['GET'])
+@admins_only
+def admin_chals():
+    chals = Challenges.query.order_by(Challenges.value).all()
+
+    json_data = {'game': []}
+    for chal in chals:
+        type_class = CHALLENGE_CLASSES.get(chal.type)
+        type_name = type_class.name if type_class else None
+
+        json_data['game'].append({
+            'id': chal.id,
+            'name': chal.name,
+            'value': chal.value,
+            'description': chal.description,
+            'category': chal.category,
+            # 'files': files,
+            # 'tags': tags,
+            # 'hints': hints,
+            'state': chal.state,
+            'max_attempts': chal.max_attempts,
+            'type': chal.type,
+            'type_name': type_name,
+            'type_data': {
+                'id': type_class.id,
+                'name': type_class.name,
+                'templates': type_class.templates,
+                'scripts': type_class.scripts,
+            }
+        })
+
+    db.session.close()
+    return jsonify(json_data)
 
 
 @admin.route('/admin/challenges/<int:challenge_id>')
